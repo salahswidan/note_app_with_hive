8b1aa0ba175ffa28a66aaa5dafae3c9be1db2c04
salah <salahswidan212@gmail.com>
2025-05-17 13:54:52 +0300

    make CustomAppBarNewNoteScreen widget

--- lib/view/new_note_screen/screen/new_note_screen.dart
+++ lib/view/new_note_screen/screen/new_note_screen.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:note_app/core/resources/color_manager.dart';
+
+import '../widgets/custom_app_bar_new_note_screen.dart';
 
 class NewNoteScreen extends StatelessWidget {
   const NewNoteScreen({super.key});
@@ -6,7 +11,23 @@ class NewNoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 25.0, left: 25.0, top: 14),
+            child: Column(
+              children: [
+                CustomAppBarNewNoteScreen(
+                  onPressedBack: () {
+                    Navigator.of(context).pop();
+                  },
+                  onPressedAtMark: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/view/new_note_screen/widgets/custom_app_bar_new_note_screen.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/resources/color_manager.dart';
+
+class CustomAppBarNewNoteScreen extends StatelessWidget {
+  const CustomAppBarNewNoteScreen({
+    super.key, required this.onPressedBack, required this.onPressedAtMark,
+  });
+  final VoidCallback onPressedBack;
+  final VoidCallback onPressedAtMark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton.icon(
+          icon: Icon(
+            CupertinoIcons.back,
+            color: ColorManager.KPrimaryColor,
+            size: 30,
+          ),
+          onPressed: onPressedBack,
+          label: Text(
+            "Back",
+            style: TextStyle(
+                color: ColorManager.KPrimaryColor,
+                fontSize: 20,
+                fontFamily: "Otama",
+                fontWeight: FontWeight.w700),
+          ),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.zero,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 7.w, vertical: 6.h),
+              fixedSize: const Size(33, 33),
+              backgroundColor: ColorManager.KPrimaryColor,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            ),
+            onPressed: onPressedAtMark,
+            child: Center(
+              child: Icon(
+                CupertinoIcons.check_mark_circled,
+                color: Colors.white,
+                size: 20.sp,
+              ),
+            ))
+      ],
+    );
+  }
+}
